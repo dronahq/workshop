066418e8b6fee19b0b668da16fc6c270bee50307
Migom Pegu <migom.08015020@gmail.com>
2017-05-29 17:19:19 +0530

    Exit App

---
+++ v2-app-exit/scripts/main.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    document.addEventListener('deviceready', function () {
+        var obj = new StartPage();
+        obj.init();
+    });
+});
+
+var StartPage = function () {
+
+    var _fnExitApp = function () {
+        DronaHQ.app.exitApp();
+    }
+
+    var _onClick = function () {
+        $('.home_backbutton').off('click').on('click', function () {
+            _fnExitApp();
+        });
+    }
+
+    return {
+        init: function () {
+            _onClick();
+        }
+    }
+}
