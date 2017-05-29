91378019e2880aabed0155b2cfbe5f2e5934dccc
Migom Pegu <migom.08015020@gmail.com>
2017-05-29 17:20:03 +0530

    User Profile

---
+++ v3-user-profile/scripts/main.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function () {
+    document.addEventListener('deviceready', function () {
+        var obj = new StartPage();
+        obj.init();
+    });
+});
+
+var StartPage = function () {
+
+    var getDronaHQUser = function () {
+
+        DronaHQ.user.getProfile(function (data) {
+            $('#userProfile').text('Welcome ' + data.name);
+            $('.page').removeClass('hide');
+        },
+          function (err) {
+          });
+    };
+
+    var _fnExitApp = function () {
+        DronaHQ.app.exitApp();
+    }
+
+    var _onClick = function () {
+        $('.home_backbutton').off('click').on('click', function () {
+            _fnExitApp();
+        });
+    }
+
+    return {
+        init: function () {
+            getDronaHQUser();
+            _onClick();
+        }
+    }
+}
